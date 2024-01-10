f408330f68f570108fd23251d7ebe6a1ec70e9df
Simon Paul <samonpaul06@gmail.com>
2024-01-10 16:53:28 +0530

    update - cards

--- src/components/main/CarsList.jsx
+++ src/components/main/CarsList.jsx
@@ -50,6 +50,28 @@ const CarsList = () => {
                 <button className="card-button">Buy Now</button>
             </div>
         </article>
+        <article className="card">
+            <img src={CardDemo2} alt="car image" className="card-image" />
+            <div className="card-details">
+                <h3 className="card-title">Car Demo #1</h3>
+                <p className="card-description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad qui magni laboriosam perspiciatis maiores velit.
+                </p>
+                <p className="card-price">$12000</p>
+                <button className="card-button">Buy Now</button>
+            </div>
+        </article>
+        <article className="card">
+            <img src={CardDemo1} alt="car image" className="card-image" />
+            <div className="card-details">
+                <h3 className="card-title">Car Demo #1</h3>
+                <p className="card-description">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad qui magni laboriosam perspiciatis maiores velit.
+                </p>
+                <p className="card-price">$12000</p>
+                <button className="card-button">Buy Now</button>
+            </div>
+        </article>
     </section>
   )
 }
